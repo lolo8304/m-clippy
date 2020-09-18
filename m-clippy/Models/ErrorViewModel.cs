4525aca4b2b6ed4cf96e29adf765a9e5adb60a29
Lorenz Hänggi <lolo8304@gmail.com>
2020-09-18 22:37:28 +0200

    initial

---
+++ m-clippy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace m_clippy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
