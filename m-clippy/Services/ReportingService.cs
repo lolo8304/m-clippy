c4c58f6fb7dea882217920f29fa37fab45f2a26c
Cédric Walter <cedric.walter@gmail.com>
2020-09-20 02:26:30 +0200

    fix bug in counter of lifestyle, improve code robustnesss

--- m-clippy/Services/ReportingService.cs
+++ m-clippy/Services/ReportingService.cs
@@ -100,12 +100,12 @@ namespace m_clippy.Services
 
                 _logger.LogDebug("Found " + cart.cartItems.Count + " cart items in purchase " + purchase.EinkaufID + " with " + productDetails.Products.Count);
 
-                foreach (ProductDetail productDetail in productDetails.Products.GetRange(0, 1))
+                foreach (ProductDetail productDetail in productDetails.Products)
                 {
                     var clippyProductDetail = new ClippyProductDetail
                     {
-                        Thumbnail = productDetail.ImageTransparent.Stack.ToString().Replace("{stack}", "small"),
-                        Image = productDetail.ImageTransparent.Stack.ToString().Replace("{stack}", "medium"),
+                        Thumbnail = productDetail?.ImageTransparent?.Stack.ToString().Replace("{stack}", "small"),
+                        Image = productDetail?.ImageTransparent?.Stack.ToString().Replace("{stack}", "medium"),
 
                         // TODO should be historical to shoppingcart date :-)
            
@@ -131,33 +131,35 @@ namespace m_clippy.Services
                         clippyProductDetail.Quantity = "1";
                     }
 
-
-                    // if it match user settings
-                    var productAllergens = productDetail?.Features.FindAll(s => s.LabelCode.Equals("MAPI_ALLERGENES"));
-                    foreach (Models.ProductDetails.Feature productAllergen in productAllergens)
+                    if (productDetail.Features != null)
                     {
-                        foreach (Value value in productAllergen.Values)
+                        var productAllergens = productDetail?.Features.FindAll(s => s.LabelCode.Equals("MAPI_ALLERGENES"));
+                        foreach (Models.ProductDetails.Feature productAllergen in productAllergens)
                         {
-                            string allergen = value.ValueCode.ToString();
-
-                            if (user.Allergies.Matching.Contains(allergen))
+                            foreach (Value value in productAllergen.Values)
                             {
-                                clippyProductDetail.AllergyAlert = true;
+                                string allergen = value.ValueCode.ToString();
 
-                                clippyProductsDetails.AllergyCounter++;
+                                if (user.Allergies.Matching.Contains(allergen))
+                                {
+                                    clippyProductDetail.AllergyAlert = true;
+
+                                    clippyProductsDetails.AllergyCounter++;
+                                }
+
+                                // keep track of all allergens
+                                int previousValue = clippyProductsDetails.allergens.GetValueOrDefault(allergen, 0);
+                                clippyProductsDetails.allergens.Remove(allergen);
+                                clippyProductsDetails.allergens.Add(allergen, previousValue++);
+
+                                clippyProductsDetails.AllergensCounter++;
                             }
-
-                            // keep track of all allergens
-                            int previousValue = clippyProductsDetails.allergens.GetValueOrDefault(allergen, 0);
-                            clippyProductsDetails.allergens.Remove(allergen);
-                            clippyProductsDetails.allergens.Add(allergen, previousValue++);
-
-                            clippyProductsDetails.AllergensCounter++;
                         }
-                    }
 
-                    if (productAllergens.Count == 0) {
-                        clippyProductsDetails.NoAllergensCounter++;
+                        if (productAllergens.Count == 0)
+                        {
+                            clippyProductsDetails.NoAllergensCounter++;
+                        }
                     }
 
                     bool sumAdded = false;
@@ -235,74 +237,82 @@ namespace m_clippy.Services
                         sumAdded = true;
                     }
 
-                    if (user.Habits.Vegan)
+              
+                    if (productDetail.Labels != null)
                     {
-                        if (productDetail.Labels != null)
+                        List<Models.ProductDetails.Label2> isVegan = productDetail?.Labels.FindAll(s => s.Name.Equals("V-vegan"));
+                        if (isVegan.Count == 0)
                         {
-                            List<Models.ProductDetails.Label2> isVegan = productDetail?.Labels.FindAll(s => s.Name.Equals("V-vegan"));
-                            if (isVegan.Count == 0)
-                            {
-                                clippyProductDetail.HabitsAlert = true;
-                                clippyProductsDetails.HabitsCounter++;
+                            clippyProductDetail.HabitsAlert = true;
+                            clippyProductsDetails.HabitsCounter++;
 
-                                clippyProductsDetails.NotVeganCounter++;
-                            }
-                            else {
-                                clippyProductsDetails.VeganCounter++;
-                            }
+                            clippyProductsDetails.NotVeganCounter++;
+                        }
+                        else
+                        {
+                            clippyProductsDetails.VeganCounter++;
+                        }
 
-                            List<Models.ProductDetails.Label2> isVeganBlume = productDetail?.Labels.FindAll(s => s.Name.Equals("Veganblume"));
-                            if (isVeganBlume.Count == 0)
-                            {
-                                clippyProductDetail.HabitsAlert = true;
-                                clippyProductsDetails.HabitsCounter++;
+                        List<Models.ProductDetails.Label2> isVeganBlume = productDetail?.Labels.FindAll(s => s.Name.Equals("Veganblume"));
+                        if (isVeganBlume.Count == 0)
+                        {
+                            clippyProductDetail.HabitsAlert = true;
+                            clippyProductsDetails.HabitsCounter++;
 
-                                clippyProductsDetails.NotVeganCounter++;
-                            }
-                            else
-                            {
-                                clippyProductsDetails.VeganCounter++;
-                            }
+                            clippyProductsDetails.NotVeganCounter++;
+                        }
+                        else
+                        {
+                            clippyProductsDetails.VeganCounter++;
                         }
                     }
-
-                    if (user.Habits.Vegetarian)
+                    else
                     {
-                        if (productDetail.Labels != null)
-                        {
-                            List<Models.ProductDetails.Label2> isVegetarian = productDetail?.Labels.FindAll(s => s.Name.Equals("V-vegetarisch"));
-                            if (isVegetarian.Count == 0)
-                            {
-                                clippyProductDetail.HabitsAlert = true;
-                                clippyProductsDetails.HabitsCounter++;
-
-                                clippyProductsDetails.NotVegetarianCounter++;
-                            }
-                            else
-                            {
-                                clippyProductsDetails.VegetarianCounter++;
-                            }
-                        }
+                        clippyProductsDetails.NotVeganCounter++;
                     }
 
-                    if (user.Habits.Bio)
-                    {
-                        if (productDetail.Labels != null)
-                        {
-                            List<Models.ProductDetails.Label2> isBIO = productDetail?.Labels.FindAll(s => s.Name.Equals("Migros Bio"));
-                            if (isBIO.Count == 0)
-                            {
-                                clippyProductDetail.HabitsAlert = true;
-                                clippyProductsDetails.HabitsCounter++;
 
-                                clippyProductsDetails.NotBioCounter++;
-                            }
-                            else
-                            {
-                                clippyProductsDetails.BioCounter++;
-                            }
+                    if (productDetail.Labels != null)
+                    {
+                        List<Models.ProductDetails.Label2> isVegetarian = productDetail?.Labels.FindAll(s => s.Name.Equals("V-vegetarisch"));
+                        if (isVegetarian.Count == 0)
+                        {
+                            clippyProductDetail.HabitsAlert = true;
+                            clippyProductsDetails.HabitsCounter++;
+
+                            clippyProductsDetails.NotVegetarianCounter++;
+                        }
+                        else
+                        {
+                            clippyProductsDetails.VegetarianCounter++;
                         }
                     }
+                    else
+                    {
+                        clippyProductsDetails.NotVegetarianCounter++;
+                    }
+
+
+                    if (productDetail.Labels != null)
+                    {
+                        List<Models.ProductDetails.Label2> isBIO = productDetail?.Labels.FindAll(s => s.Name.Equals("Migros Bio"));
+                        if (isBIO.Count == 0)
+                        {
+                            clippyProductDetail.HabitsAlert = true;
+                            clippyProductsDetails.HabitsCounter++;
+
+                            clippyProductsDetails.NotBioCounter++;
+                        }
+                        else
+                        {
+                            clippyProductsDetails.BioCounter++;
+                        }
+                    }
+                    else
+                    {
+                        clippyProductsDetails.NotBioCounter++;
+                    }
+
 
                     clippyProductsDetails.list.Add(clippyProductDetail);
                 }
@@ -311,8 +321,8 @@ namespace m_clippy.Services
             // we just count the unique number of different countries from all articles bought by customer
             clippyProductsDetails.CountriesCounter = CountriesSet.Count;
 
-            clippyProductsDetails.CarKm = r.Next(0, 100) + " km";
-            clippyProductsDetails.PlanesKm = r.Next(0, 500) + " km";
+            clippyProductsDetails.CarKm = r.Next(5, 100) + " km";
+            clippyProductsDetails.PlanesKm = r.Next(10, 500) + " km";
 
 
             // Naive counter impl
